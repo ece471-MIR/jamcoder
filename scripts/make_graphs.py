85614dd1c21e9412525960f9d178dc608dc44ffe
JAMES RYAN <james.paul9889@gmail.com>
2025-12-14 22:18:20 -0500

    scripts: added plotter for mushra

---
+++ scripts/make_graphs.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('./results.csv')
+examinee_list = df['session_uuid'].unique()
+df = df[['trial_id', 'rating_stimulus', 'rating_score']]
+
+# aggregate data
+audiorefs = ['A', 'B', 'C', 'D', 'reference']
+
+trial_results: list[pd.DataFrame] = []
+for t in range(1,10):   # 9 trials
+    trial_df = df.query(f'trial_id == "trial{t}"')
+    newdf = pd.DataFrame()
+    for ref in audiorefs:
+        ref_results = trial_df.query(f'rating_stimulus == "{ref}"')['rating_score'].values
+        newdf.insert(0, ref, ref_results)
+    trial_results.append(newdf)
+    plt.figure()
+    boxplot = trial_results[t-1].boxplot(column=audiorefs)
+    plt.title(f'Trial {t} MOS Results')
+    plt.savefig(f'./trial{t}_results.png')
