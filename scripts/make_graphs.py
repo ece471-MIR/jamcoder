241b939cc62e0d9a97064685c5a5a424e72754a8
JAMES RYAN <james.paul9889@gmail.com>
2025-12-18 05:04:42 -0500

    make_graphs: produces the corpora results cuz more useful lol

--- scripts/make_graphs.py
+++ scripts/make_graphs.py
@@ -10,18 +10,26 @@ df = df[['trial_id', 'rating_stimulus', 'rating_score']]
 # aggregate data
 audiorefs = ['A', 'B', 'C', 'D', 'reference']
 
+ant = [1, 2, 3]
+james = [4, 5, 6]
+meg = [7, 8, 9]
+
+trialgroups = [ant, james, meg]
+names = ['Ant', 'James', 'Megan']
+
 trial_results: list[pd.DataFrame] = []
-for t in range(1,10):   # 9 trials
-    trial_df = df.query(f'trial_id == "trial{t}"')
+for t in range(0,3):
+    trial_df = df.query(f'trial_id == "trial{trialgroups[t][0]}" | trial_id == "trial{trialgroups[t][1]}" | trial_id == "trial{trialgroups[t][2]}"')
+    breakpoint()
     newdf = pd.DataFrame()
     for ref in audiorefs:
         ref_results = trial_df.query(f'rating_stimulus == "{ref}"')['rating_score'].values
         newdf.insert(0, ref, ref_results)
     trial_results.append(newdf)
     plt.figure()
-    boxplot = trial_results[t-1].boxplot(column=audiorefs)
+    boxplot = trial_results[t].boxplot(column=audiorefs)
     plt.ylim([0, 100])
-    plt.title(f'Trial {t} MOS Results')
-    plt.savefig(f'./trial{t}_results.png')
+    plt.title(f'{names[t]} Corpus MOS for Intelligibility')
+    plt.savefig(f'./group{t}_results.png')
 
 print(f'Number of responses: {examinee_num}')
